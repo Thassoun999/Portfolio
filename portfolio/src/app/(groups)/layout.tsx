3bf773376a3cd125b5fbda9315e694532ef82499
Tae (Sydra) HAssoun <thassoun000@gmail.com>
2025-11-19 22:24:51 -0500

    Nav Start

--- portfolio/src/app/(groups)/layout.tsx
+++ portfolio/src/app/(groups)/layout.tsx
@@ -1,7 +1,42 @@
+import NavButton from "@/components/NavButton";
 export default function GroupLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <section>{children}</section>;
+  const Image1 = [
+    "/Media/words1_new_unhover_transparent.gif",
+    "/Media/words1_new_hover_transparent.gif",
+  ];
+  const Image2 = [
+    "/Media/words2_new_unhover_transparent.gif",
+    "/Media/Words2_new_hover_transparent.gif",
+  ];
+  const Image3 = [
+    "/Media/words3_new_unhover_transparent.gif",
+    "/Media/words3_new_hover_transparent.gif",
+  ];
+  return (
+    <>
+      <div className="flex flex-row justify-center mt-2 mx-auto w-6/7 border border-t-white">
+        <NavButton
+          hrefLink="about"
+          pathnameUnhover={Image1[0]}
+          pathnameHover={Image1[1]}
+          className="mt-2"
+        />
+        <NavButton
+          hrefLink="/contact"
+          pathnameUnhover={Image2[0]}
+          pathnameHover={Image2[1]}
+        />
+        <NavButton
+          hrefLink="/work"
+          pathnameUnhover={Image3[0]}
+          pathnameHover={Image3[1]}
+        />
+      </div>
+      {children}
+    </>
+  );
 }
---
+++ portfolio/src/components/NavButton.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import Image from "next/image";
+
+export type NavButtonProps = {
+  pathnameUnhover: string;
+  pathnameHover: string;
+  hrefLink: string;
+  className?: string;
+};
+
+export default function NavButtonProps({
+  pathnameHover,
+  pathnameUnhover,
+  hrefLink,
+  className = "",
+}: NavButtonProps) {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <Link
+      href={hrefLink}
+      key={`Image-${hover}`}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      className="relative w-full aspect-3/2"
+    >
+      <Image
+        src={pathnameUnhover}
+        alt="NavButtonImage"
+        fill
+        unoptimized
+        className={twMerge(
+          className,
+          `object-contain object-center transition-opacity duration-150 ${
+            hover ? "opacity-0" : "opacity-100"
+          }`
+        )}
+      />
+
+      <Image
+        src={pathnameHover}
+        alt="NavButtonImage"
+        fill
+        unoptimized
+        className={twMerge(
+          className,
+          `object-contain object-center transition-opacity duration-150 ${
+            hover ? "opacity-100" : "opacity-0"
+          }`
+        )}
+      />
+    </Link>
+  );
+}
