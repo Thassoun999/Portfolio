c31ec413310ff43c93121f50cca4cc8e32e60efb
Tae Hassoun <thassoun000@gmail.com>
2026-05-29 16:07:54 -0400

    Palestine

--- src/app/(groups)/about/page.tsx
+++ src/app/(groups)/about/page.tsx
@@ -12,16 +12,16 @@ export default function About() {
           <AboutBanner className="w-[177.5px] h-[130.5px] sm:w-[236.6px] sm:h-[174px]" />
 
           <p className="text-2xl 2xl:text-3xl pl-4 w-full space-y-2 sm:space-y-4">
-            Sydra (Tae) Hassoun is a Trans Lebanese-American Multidisciplinary
-            Artist and Creative Programmer based in NYC. She has always strived
-            to make interactive canvases that place heavy emphasis on play and
-            wonder. This can be found through her games, her multi-dimesional
-            art, and even her website work. Backwards Sunrise is both a
-            continuation of this practice and a call to radical action. Her
-            current direction is to the roots of her people, the empowerment of
-            her community, and creative liberation. The goal on the horizon is a
-            transformation of the interconnected web, one that can foster
-            empathy and revolution.
+            Sydra (Tae) Hassoun is a Trans Lebanese-Palestinian
+            Multidisciplinary Artist and Creative Programmer based in NYC. She
+            has always strived to make interactive canvases that place heavy
+            emphasis on play and wonder. This can be found through her games,
+            her multi-dimesional art, and even her website work. Backwards
+            Sunrise is both a continuation of this practice and a call to
+            radical action. Her current direction is to the roots of her people,
+            the empowerment of her community, and creative liberation. The goal
+            on the horizon is a transformation of the interconnected web, one
+            that can foster empathy and revolution.
             <br />
             <br />
             Sydra has over 6 years of professional experience and is currently
